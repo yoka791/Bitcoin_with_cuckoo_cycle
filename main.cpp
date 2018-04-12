3f6975d119c70f9bc9682943a2b68999a1a5cc22
yoka791 <karl_yosi@msn.com>
2018-04-12 14:43:30 +0300

    added main for tests, change preprocessor from edges to nodes

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string>
+#include <unistd.h>
+#include "CuckooMiner.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	int c;
+	string header = "hello world";
+	uint edge_precentage  = 70;
+
+	while ((c = getopt(argc, argv, "e:h:")) != -1) {
+		switch (c) {
+		case 'e':
+			edge_precentage = atoi(optarg);
+			break;
+		case 'h':
+			header = optarg;
+			break;
+		}
+	}
+	assert(edge_precentage >= 0 && edge_precentage <= 100);
+	printf("Looking for %d-cycle on cuckoo%d(\"%s\") with %d%% edges\n",
+		PROOFSIZE, NODEBITS, header.c_str(), edge_precentage);
+
+	array<edge_t, 42> solution_arr;
+	CuckooMiner cuc_miner(header, edge_precentage);
+	if (cuc_miner.isSolutionFound()) {
+		cuc_miner.getSolution(solution_arr);
+		printf("nonces added to array");
+	}
+
+}
