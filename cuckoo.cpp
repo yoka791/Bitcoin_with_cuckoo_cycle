5cf1972350d5bfde81c335b8d4539884e89f2620
yoka791 <karl_yosi@msn.com>
2018-04-24 20:56:42 +0300

    added verifying solution

--- cuckoo.cpp
+++ cuckoo.cpp
@@ -22,6 +22,57 @@ bool verify(const std::array<edge_t, 42> solution_arr, const std::string& header
 	siphash_keys sip_keys;
 	setKeysFromHeader(header, &sip_keys);
 
+	node_t even_nodes[ PROOFSIZE];
+	node_t odd_nodes[PROOFSIZE];
+	node_t xor0 = 0, xor1 = 0;
+	for (u32 n = 0; n < PROOFSIZE; n++) {
+		if (solution_arr[n] > MODULU)  //no. of edge does not exist
+			return false;
+		if (n && solution_arr[n] <= solution_arr[n - 1])
+			return false;
+		xor0 ^= even_nodes[n] = sipnode(&sip_keys, solution_arr[n], 0);
+		xor1 ^= odd_nodes[n] = sipnode(&sip_keys, solution_arr[n], 1);
+	}
+	if (xor0 | xor1)              // optional check for obviously bad proofs
+		return false;
+
+	node_t first_even_index = even_nodes[0];
+	int index = 0;
+	for (size_t i = 0; i < PROOFSIZE; i++)
+	{
+		if (i != 0) {
+			node_t even_val_to_search = even_nodes[index];
+			even_nodes[index] = 0;
+			size_t j;
+			for (j = 0; j < PROOFSIZE; j++)//even
+			{
+				if (even_nodes[j] == even_val_to_search) {
+					index = j;
+					break;
+				}
+			}
+			if (j == PROOFSIZE) {
+				return even_val_to_search == first_even_index ? true : false;
+			}
+		}
+		even_nodes[index] = 0;
+
+		node_t odd_val_to_search = odd_nodes[index];
+		odd_nodes[index] = 0;
+		size_t k;
+		for (k = 0; k < PROOFSIZE; k++)//odd
+		{
+			if (odd_nodes[k] == odd_val_to_search)
+			{
+				index = k;
+				odd_nodes[k] = 0;
+				break;
+			}
+		}
+	}
+	//return true;
+
+	/*
 	node_t uvs[2 * PROOFSIZE];
 	node_t xor0 = 0, xor1 = 0;
 	for (u32 n = 0; n < PROOFSIZE; n++) {
@@ -34,6 +85,9 @@ bool verify(const std::array<edge_t, 42> solution_arr, const std::string& header
 	}
 	if (xor0 | xor1)              // optional check for obviously bad proofs
 		return false;
+	*/
+	
+	/*
 	u32 n = 0, i = 0, j;
 	do {                        // follow cycle
 		for (u32 k = j = i; (k = (k + 2) % (2 * PROOFSIZE)) != i; ) {
@@ -48,6 +102,7 @@ bool verify(const std::array<edge_t, 42> solution_arr, const std::string& header
 		n++;
 	} while (i != 0);           // must cycle back to start or we would have found branch
 	return n == PROOFSIZE ? true : false;
+	*/
 }
 
 
